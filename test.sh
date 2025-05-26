b963660acf6a8b294faeff8b070c90f0fdf2db0b
mazelka <mary.banshee@gmail.com>
2025-05-26 14:34:04 +0300

    Add test wf
    Signed-off-by: mazelka <mary.banshee@gmail.com>

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@ set -e
 
 echo "Running Rails setup and tests in environment: $RAILS_ENV"
 
-bundle exec rails db:create db:schema:load
-bundle exec rspec
+bundle exec rails db:drop db:create db:schema:load
+bundle exec rspec > rspec.log 2>&1
+cat rspec.log 
